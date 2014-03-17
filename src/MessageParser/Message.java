9180d7dd4960dd8766cda40367b746ae1ff6747c
Rohan Rayarikar <rohanrayarikar@yahoo.com>
2014-03-17 02:11:26 -0400

    made all variables public

--- src/MessageParser/Message.java
+++ src/MessageParser/Message.java
@@ -11,43 +11,6 @@ public class Message {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-//		// TODO Auto-generated method stub
-//		PlayerToReferee p2r = new PlayerToReferee();
-//		System.out.println("The initial board congif is :");
-//		Map<String, String> currentBoardConfig = p2r.getInitialConfiguration();
-//		p2r.printBoardPositionsToStandardOutput(currentBoardConfig);
-//		currentBoardConfig = p2r.sendMoveToReferee(currentBoardConfig, "A1", "A10");
-//		System.out.println("\nThe modified board congif is :");
-//		p2r.printBoardPositionsToStandardOutput(currentBoardConfig);
-//		
-////		--------------------------------------------------------------
-//		
-//		System.out.println();
-//		
-//		RefereeToPlayer r2p = new RefereeToPlayer();
-//		List b = r2p.processRefereeMessage("(go 1)");
-//		System.out.println("<turn>: (go 1)" + "  valid: " + b);
-//		
-//		b = r2p.processRefereeMessage("(end 0)");
-//		System.out.println("<game_end>: (end 0)" + "  valid: " + b);
-//		
-//		b = r2p.processRefereeMessage("(flag 1 A5)");
-//		System.out.println("<flag>: (flag 1 A3)" + "  valid: " + b);
-//		
-//		b = r2p.processRefereeMessage("(init 2 time/move 10)");
-//		System.out.println("\n\n<msg0>: (init 2 time/move 10)" + "  valid: " + b);
-//		
-//		b = r2p.processRefereeMessage("(illegal (A1 A12))");
-//		System.out.println("<illegal>: (illegal (A1 A12))" + "  valid: " + b);		
-//		
-//		b = r2p.processRefereeMessage("(outcome (A1 A12))");
-//		System.out.println("<outcome>: (outcome (A1 A12))" + "  valid: " + b);
-//		
-//		b = r2p.processRefereeMessage("(outcome > (A1 A12))");
-//		System.out.println("<outcome>: (outcome > (A1 A12))" + "  valid: " + b);
-//		
-//		System.out.println();
-		
 		GamePlay game = new GamePlay();
 		try {
 			List commands = game.readCommands();
